506747b80878267293aa953b21b491f16ff914b5
Mikira Sora <mikirasora0409@126.com>
2019-12-25 11:59:42 +0800

    init commit

---
+++ BossKeyPluginInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wbooru.PluginExt;
+
+namespace WbooruPlugin.BossKey
+{
+    public class BossKeyPluginInfo : PluginInfo
+    {
+        public override string PluginName => "BossHotKey";
+
+        public override string PluginProjectWebsite => throw new NotImplementedException();
+
+        public override string PluginAuthor => throw new NotImplementedException();
+
+        public override string PluginDescription => throw new NotImplementedException();
+    }
+}
